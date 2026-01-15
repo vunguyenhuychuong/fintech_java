92f858b4ad6e8fde665da6f8844c728a31ffa6ef
vunguyenhuychuong <vunguyenhuychuong10t2@gmail.com>
2026-01-15 09:35:49 +0700

    init source

---
+++ src/main/java/com/example/fintech_java/FintechJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.fintech_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintechJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fintech_java/FintechJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fintech_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
