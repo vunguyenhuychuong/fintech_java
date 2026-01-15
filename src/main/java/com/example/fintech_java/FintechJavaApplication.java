925f1a62b07ca6bc26f7d868dd7963392c49b075
vunguyenhuychuong <vunguyenhuychuong10t2@gmail.com>
2026-01-15 14:04:28 +0700

    implement test

--- src/main/java/com/example/fintech_java/FintechJavaApplication.java
+++ src/main/java/com/example/fintech_java/FintechJavaApplication.java
@@ -2,9 +2,7 @@ package com.example.fintech_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
